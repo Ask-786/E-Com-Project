ce781337a2bf1ba30089c6caa799f1cc9019242c
Ahmad Shabeer K <Ahmadshabeerk786@gmail.com>
2022-12-16 19:06:39 +0530

    changed the date formatting from toISOString to just format to test the issue with querying data from sales report

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -657,9 +657,9 @@ const postEditCategory = async (req, res, next) => {
 
 const postQueriedSalesReport = async (req, res, next) => {
   const { from, to } = req.body;
-  const now = moment().toISOString();
-  const formattedFrom = moment(from).toISOString();
-  const formattedTo = moment(to).toISOString();
+  const now = moment().format();
+  const formattedFrom = moment(from).format();
+  const formattedTo = moment(to).format();
   if (formattedTo <= now) {
     const orders = await Order.find({
       createdAt: { $gt: formattedFrom, $lt: formattedTo },
